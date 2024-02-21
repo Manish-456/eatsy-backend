bfcfd440a5ae7e9bad3c6dd28d5784f57e67edef
Manish <tamangmanish3456@gmail.com>
2024-02-21 14:20:35 +0545

    added: user request validation middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import UserController from '../controllers/user.controller';
 import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
 router.post('/', jwtCheck, UserController.createUser);
-router.put('/', jwtCheck, jwtParse, UserController.updateUser);
+router.put('/', jwtCheck, jwtParse, validateUserRequest, UserController.updateUser);
 
 export default router;
