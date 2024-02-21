bc899279d9f615f992da173667110ea65f933256
Manish <tamangmanish3456@gmail.com>
2024-02-21 13:24:30 +0545

    added: jwt-check middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import UserController from '../controllers/user.controller';
+import { jwtCheck } from '../middleware/auth';
 
 const router = express.Router();
 
-router.post('/', UserController.createUser);
+router.post('/', jwtCheck, UserController.createUser);
 
 export default router;
